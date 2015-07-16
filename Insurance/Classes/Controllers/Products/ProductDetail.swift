483a1246ec5b592b6201b232a9450f5d43ac4a78
NY <nypisces@msn.com>
2015-07-16 10:33:37 +0800

    调整目录结构

---
+++ Insurance/Classes/Controllers/Products/ProductDetail.swift
@@ -0,0 +1,33 @@
+//
+//  Copyright (c) 2015年 NY. All rights reserved.
+//
+
+class ProductDetail: TableDetail {
+    // MARK: - 🐤 Taylor
+    override func onPrepare() {
+        super.onPrepare()
+        items = [[
+            Item(title: "price"),
+            Item(title: "created_time")
+            ]]
+        refreshMode = .DidLoad
+    }
+    
+    override func onCreateLoader() -> BaseLoader? {
+        let mapping = smartMapping(Order.self)
+        return HttpLoader(endpoint: endpoint, mapping: mapping)
+    }
+    
+    override func getItemView<T : Order, C : UITableViewCell>(data: T, tableView: UITableView, indexPath: NSIndexPath, item: Item, cell: C) -> UITableViewCell {
+        switch item.title {
+        case "price":
+            let formatter = NSNumberFormatter()
+            formatter.numberStyle = .CurrencyStyle
+            cell.detailTextLabel?.text = formatter.stringFromNumber(NSNumber(double: data.price.doubleValue / 100))
+        case "created_time":
+            cell.detailTextLabel?.text = TTTTimeIntervalFormatter().stringForTimeInterval(data.valueForKey(item.title.camelCaseString())!.timeIntervalSinceNow)
+        default: break
+        }
+        return cell
+    }
+}
