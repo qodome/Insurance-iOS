b6b7a8fe13790be24e900d5bd2f4c28539a0b272
jinxianxian <530654814@qq.com>
2015-11-06 20:32:59 +0800

    SCREEN_WITH问题

--- Insurance/Classes/Controllers/GDS/EnquiryWaiting.swift
+++ Insurance/Classes/Controllers/GDS/EnquiryWaiting.swift
@@ -7,20 +7,19 @@ class EnquiryWaiting: GroupedTableDetail, UIAlertViewDelegate {
     override func onPrepare() {
         super.onPrepare()
         mapping = smartMapping(Enquiry.self)
-        LOG(data)
         tableView.backgroundColor = .whiteColor()
-        let imageView = ImageView(frame: CGRectMake(SCREEN_WIDTH / 6, SCREEN_WIDTH / 6, SCREEN_WIDTH / 3 * 2, SCREEN_WIDTH / 3 * 2), cornerRadius: SCREEN_WIDTH / 3)
+        let imageView = ImageView(frame: CGRectMake(view.frame.width / 6, view.frame.width / 6, view.frame.width / 3 * 2, view.frame.width / 3 * 2), cornerRadius: view.frame.width / 3)
         imageView.image = UIImage(named: endpoint.containsString("orders") ? "ic_order.png" : "ic_wait.png")
         tableView.addSubview(imageView)
-        let button = getButton(CGRectMake((SCREEN_WIDTH - 160) / 2, view.frame.height - TAB_BAR_HEIGHT - 2 * PADDING - BUTTON_HEIGHT - 64, 160, BUTTON_HEIGHT), title: endpoint.containsString("orders") ? LocalizedString("查看详情") : LocalizedString("取消询价"), theme: Theme(type: .Light, color: endpoint.containsString("orders") ? APP_COLOR : 0xB2B2B1))
+        let button = getButton(CGRectMake((view.frame.width - 160) / 2, view.frame.height - TAB_BAR_HEIGHT - 2 * PADDING - BUTTON_HEIGHT - 64, 160, BUTTON_HEIGHT), title: endpoint.containsString("orders") ? LocalizedString("查看详情") : LocalizedString("取消询价"), theme: Theme(type: .Light, color: endpoint.containsString("orders") ? APP_COLOR : 0xB2B2B1))
         button.addTarget(self, action: "cancle", forControlEvents: .TouchUpInside)
         tableView.addSubview(button)
-        let detailLabel = UILabel(frame: CGRectMake(PADDING, 0, SCREEN_WIDTH - 2 * PADDING, 0))
+        let detailLabel = UILabel(frame: CGRectMake(PADDING, 0, view.frame.width - 2 * PADDING, 0))
         detailLabel.text = endpoint.containsString("orders") ? "订单有效期 72 小时，保险机构将尽快与您联系并完成投保，请保持手机畅通" : "询价发起时间 \(getString("createTime"))，保险机构正为您报价，请在发起时间2小时后查看报价"
         detailLabel.numberOfLines = 0
         detailLabel.textAlignment = .Center
         detailLabel.sizeToFit()
-        detailLabel.center = CGPointMake(SCREEN_WIDTH / 2, button.frame.origin.y - 2 * PADDING - detailLabel.frame.height / 2)
+        detailLabel.center = CGPointMake(view.frame.width / 2, button.frame.origin.y - 2 * PADDING - detailLabel.frame.height / 2)
         tableView.addSubview(detailLabel)
     }
     
